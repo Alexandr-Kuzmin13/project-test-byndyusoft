5b84f2ce1f2a41a9ab304e813b3b02b9b9d6d5d5
Alexandr Kuzmin <rusglagol@gmail.com>
2023-04-07 15:33:13 +0300

    first commit

---
+++ src/main/java/org/example/IntervalArrayAnalysis.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.logging.Logger;
+
+public class IntervalArrayAnalysis {
+
+    private static final Logger logger = Logger.getLogger(IntervalArrayAnalysis.class.getName());
+
+    public static int sumTwoMin(Object[] numbers) throws IncorrectDataException {
+
+        List<Predicate<Object>> predicates = new ArrayList<>();
+
+        predicates.add(x -> Arrays.equals((Object[]) x, new Object[]{}));
+        predicates.add(x -> !(Arrays.stream(((Object[]) x))
+            .allMatch(y -> y instanceof Integer)));
+        predicates.add(x -> ((Object[]) x).length > Integer.MAX_VALUE - 2);
+
+        for (var predicate : predicates) {
+            if (predicate.test(numbers)) {
+                throw new IncorrectDataException("Couldn't calculate the amount");
+            }
+        }
+
+        Arrays.sort(numbers);
+        logger.info("calculation result = " + ((int) numbers[0] + (int) numbers[1]));
+        return (int) numbers[0] + (int) numbers[1];
+    }
+}
---
+++ src/test/java/org/example/IntervalArrayAnalysisTest.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class IntervalArrayAnalysisTest {
+
+    @Test
+    void testsumTwoMin() throws Exception {
+        Object[] numbers = {-12, 21, 9, 15, 25, 25, 30, 1, 5, 9};
+        var result = IntervalArrayAnalysis.sumTwoMin(numbers);
+        assertThat(result).isEqualTo(-11);
+
+        Object[] numbers1 = {};
+        Throwable thrown = assertThrows(Exception.class, () -> IntervalArrayAnalysis.sumTwoMin(numbers1));
+        assertThat(thrown.getMessage()).isEqualTo("Couldn't calculate the amount");
+
+        Object[] numbers2 = {"-12", "21", "9"};
+        Throwable thrown1 = assertThrows(Exception.class, () -> IntervalArrayAnalysis.sumTwoMin(numbers2));
+        assertThat(thrown1.getMessage()).isEqualTo("Couldn't calculate the amount");
+
+    }
+}
